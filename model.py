f23f0bd9c2b208eb497f87c2bfb9ff31cdffafc5
Peter Zinkovsky <peterz3@example.com>
2019-09-03 15:28:17 -0500

    added dataset to model

---
+++ model.py
@@ -0,0 +1,54 @@
+
+import datetime
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.callbacks import TensorBoard, LearningRateScheduler
+
+import resnet
+
+
+NUM_GPUS = 2
+BS_PER_GPU = 128
+NUM_EPOCHS = 60
+
+HEIGHT = 32
+WIDTH = 32
+NUM_CHANNELS = 3 
+NUM_CLASSES = 10
+NUM_TRAIN_SAMPLES = 50000
+
+BASE_LEARNING_RATE = 0.1
+LR_SCHEDULE = [(0.1, 30), (0.01, 45)]
+
+
+def normalize(x, y):
+  x = tf.image.per_image_standardization(x)
+  return x, y
+
+
+def augmentation(x, y):
+    x = tf.image.resize_with_crop_or_pad(
+        x, HEIGHT + 8, WIDTH + 8)
+    x = tf.image.random_crop(x, [HEIGHT, WIDTH, NUM_CHANNELS])
+    x = tf.image.random_flip_left_right(x)
+    return x, y	
+
+
+def schedule(epoch):
+  initial_learning_rate = BASE_LEARNING_RATE * BS_PER_GPU / 128
+  learning_rate = initial_learning_rate
+  for mult, start_epoch in LR_SCHEDULE:
+    if epoch >= start_epoch:
+      learning_rate = initial_learning_rate * mult
+    else:
+      break
+  tf.summary.scalar('learning rate', data=learning_rate, step=epoch)
+  return learning_rate
+
+
+(x,y), (x_test, y_test) = keras.datasets.cifar10.load_data()
+
+train_dataset = tf.data.Dataset.from_tensor_slices((x,y))
+test_dataset = tf.data.Dataset.from_tensor_slices((x_test, y_test))
+
